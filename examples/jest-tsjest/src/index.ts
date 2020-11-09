82e0c18bae337651fbcf7eb68d8d825661ec33c5
Arsen A. Gutsal <gutsal.arsen@gmail.com>
2020-11-09 12:16:29 +0200

    initial http server commit

--- examples/jest-tsjest/src/index.ts
+++ examples/jest-tsjest/src/index.ts
@@ -1,3 +1,21 @@
-import { meaningOfLife } from "@nighttrax/foo";
+import { createServer, IncomingMessage, ServerResponse } from 'http';
 
-export const whatIsTheMeaningOfLife = () => meaningOfLife;
+const port = 5000;
+
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+    response.end(JSON.stringify(request.headers));
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+// import * as http from 'http' const requestListener = function (req, res) {
+// res.writeHead(200); res.end('Hello, World!'); console.log(req, res); }
+
+// const server = http.createServer(requestListener);
+// server.port = 8080;
+// console.log('Server started on ${server.port}')
+// server.listen(server.port);
+
+export const whatIsTheMeaningOfLife = server;
