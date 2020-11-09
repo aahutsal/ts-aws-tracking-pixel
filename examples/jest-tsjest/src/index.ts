1121a1f3fbafb76c689dd331d463066d6b7e311f
EC2 Default User <ec2-user@ip-172-31-93-100.ec2.internal>
2020-11-09 17:11:54 +0000

    code fixed. geo location is determined

--- examples/jest-tsjest/src/index.ts
+++ examples/jest-tsjest/src/index.ts
@@ -1,21 +1,18 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { lookup } from 'geoip-lite'
 
 const port = 5000;
 
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
-    response.end(JSON.stringify(request.headers));
+    const obj:any = request.headers
+    obj.geo = lookup(obj.remoteAddress = request.socket.remoteAddress)
+    const json:string = JSON.stringify(obj)
+    console.log(json);
+    response.end(json);
 });
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
-
-// import * as http from 'http' const requestListener = function (req, res) {
-// res.writeHead(200); res.end('Hello, World!'); console.log(req, res); }
-
-// const server = http.createServer(requestListener);
-// server.port = 8080;
-// console.log('Server started on ${server.port}')
-// server.listen(server.port);
 
 export const whatIsTheMeaningOfLife = server;
