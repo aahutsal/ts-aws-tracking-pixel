fcb9dbaec9ee95097ea984e18e76be369d4aa4c4
Arsen A. Gutsal <gutsal.arsen@gmail.com>
2020-11-16 16:43:22 +0200

    editorconfig applied, indent-size now is 2 spaces

--- examples/jest-tsjest/src/index.ts
+++ examples/jest-tsjest/src/index.ts
@@ -3,54 +3,54 @@ import { lookup } from 'geoip-lite'
 import * as winston from 'winston'
 
 const logger = winston.createLogger({
-      level: 'info',
-      format: winston.format.json(),
-      defaultMeta: { service: 'user-service' },
-      transports: [
-            //
-            // - Write all logs with level `error` and below to `error.log`
-            // - Write all logs with level `info` and below to `combined.log`
-            //
-            new winston.transports.File({ filename: 'error.log', level: 'error' }),
-            new winston.transports.File({ filename: 'combined.log' }),
-          ],
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
 });
- 
+
 //
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
 if (process.env.NODE_ENV !== 'production') {
-      logger.add(new winston.transports.Console({
-            format: winston.format.simple(),
-          }));
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
 }
 
 
 const port = 5000
 
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
-    const cache = []
-    const endless = (key, value) => {
-        if (typeof value === 'object' && value !== null) {
-            // Duplicate reference found, discard key
-            if (cache.includes(value)) return
+  const cache = []
+  const endless = (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      // Duplicate reference found, discard key
+      if (cache.includes(value)) return
 
-            // Store value in our collection
-            cache.push(value)
-        }
-        return value
+      // Store value in our collection
+      cache.push(value)
     }
-    console.info(JSON.stringify(request, endless))
+    return value
+  }
+  console.info(JSON.stringify(request, endless))
   const obj:any = request.headers
   obj.geo = lookup(obj.remoteAddress = request.socket.remoteAddress)
-    const json:string = JSON.stringify(obj, endless)
-    logger.info(json)
-    response.writeHead(302, {
-        'Location': 'https://bit.ly/' + request.url
+  const json:string = JSON.stringify(obj, endless)
+  logger.info(json)
+  response.writeHead(302, {
+    'Location': 'https://bit.ly/' + request.url
 
-    });
-    response.end()
+  });
+  response.end()
 })
 
 server.listen(port, () => {
