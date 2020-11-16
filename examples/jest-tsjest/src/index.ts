4edb89c7888ff953283038f233e3c2e2e2a86417
Arsen A. Gutsal <gutsal.arsen@gmail.com>
2020-11-16 12:08:41 +0200

    code reworked to redirect. now use winston logger

--- examples/jest-tsjest/src/index.ts
+++ examples/jest-tsjest/src/index.ts
@@ -1,29 +1,61 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { createServer, IncomingMessage, ServerResponse } from 'http'
 import { lookup } from 'geoip-lite'
+import * as winston from 'winston'
 
-const port = 5000;
+const logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.json(),
+      defaultMeta: { service: 'user-service' },
+      transports: [
+            //
+            // - Write all logs with level `error` and below to `error.log`
+            // - Write all logs with level `info` and below to `combined.log`
+            //
+            new winston.transports.File({ filename: 'error.log', level: 'error' }),
+            new winston.transports.File({ filename: 'combined.log' }),
+          ],
+});
+ 
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+      logger.add(new winston.transports.Console({
+            format: winston.format.simple(),
+          }));
+}
+
+
+const port = 5000
 
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
-  const cache = [];
-  console.log(JSON.stringify(request, (key, value) => {
-    if (typeof value === 'object' && value !== null) {
-      // Duplicate reference found, discard key
-      if (cache.includes(value)) return;
+    const cache = []
+    const endless = (key, value) => {
+        if (typeof value === 'object' && value !== null) {
+            // Duplicate reference found, discard key
+            if (cache.includes(value)) return
 
-      // Store value in our collection
-      cache.push(value);
+            // Store value in our collection
+            cache.push(value)
+        }
+        return value
     }
-    return value;
-  }))
+    console.info(JSON.stringify(request, endless))
   const obj:any = request.headers
   obj.geo = lookup(obj.remoteAddress = request.socket.remoteAddress)
   const json:string = JSON.stringify(obj)
-  console.log(json);
-  response.end(json);
-});
+    logger.debug(json)
+    response.writeHead(302, {
+        'Location': 'https://bit.ly/' + request.url
+
+    });
+    response.end()
+    //response.end(JSON.stringify(request, endless))
+})
 
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  logger.debug(`Server listening on port ${port}`)
+})
 
-export const whatIsTheMeaningOfLife = server;
+export const app = server
--- examples/jest-tsjest/test/index.spec.ts
+++ examples/jest-tsjest/test/index.spec.ts
@@ -1,7 +1,20 @@
-import { whatIsTheMeaningOfLife } from "../src";
+import { app } from "../src";
+const supertest = require('supertest')
+const request = supertest(app)
 
 describe("meaning of life", () => {
-  it("should be 42", () => {
-    expect(whatIsTheMeaningOfLife()).toEqual(42);
+
+    afterAll(async done => {
+        app.close()
+        done()
+    })
+    it("should access server and receive JSON",  async () => {
+      // Sends GET Request to /test endpoint
+        return request
+            .get('/xxx')
+            .send()
+            .expect(302)
+            .expect('Location', 'https://bit.ly/xxx'o)
+  
   });
 });
